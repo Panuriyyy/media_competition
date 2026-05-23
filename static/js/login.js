d402616ce175dac4a494204ca113d39649796dab
Panuriyyy <tanyatv22@gmail.com>
2026-05-23 07:20:04 +0300

    Fix API_URL: use relative path instead of localhost

--- static/js/login.js
+++ static/js/login.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://127.0.0.1:8001'; 
+const API_URL = '';
 
 document.addEventListener('DOMContentLoaded', function() {
     const token = localStorage.getItem('token');
--- static/js/user.js
+++ static/js/user.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://127.0.0.1:8001';
+const API_URL = '';
 let currentTasks = [];
 let currentTaskId = null;
 let currentTaskType = null;
